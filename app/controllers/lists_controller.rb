619351f98be86acfbceeb0743a3b9acf18d99ed8
Mike Johns <mikejohns7@icloud.com>
2020-01-25 10:05:18 -0800

    Tweaking list demo

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,8 +7,22 @@ class ListsController < ApplicationController
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new list_params
+    @list.user = current_user
+    if @list.save
+      redirect_to @list
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resource :session, controller: "clearance/sessions", only: [:create]
 
   resources :lists
+  resources :tasks
 
   resources :users, controller: "clearance/users", only: [:create] do
     resource :password,
@@ -14,5 +15,7 @@ Rails.application.routes.draw do
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
+
+  root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
